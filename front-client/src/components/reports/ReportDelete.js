1d585cf0ca3e4051cbd5d5168ccd0d6b35d02cf5
Rich <rpavesi84@gmail.com>
2020-02-16 16:16:06 -0500

    creates delete

---
+++ front-client/src/components/reports/ReportDelete.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import Modal from '../Modal'
+import history from  '../../history'
+import {fetchReport, deleteReport} from '../../actions'
+
+class ReportDelete extends React.Component {
+
+  componentDidMount(){
+    this.props.fetchReport(this.props.match.params.id)
+  }
+
+
+  renderActions(){
+
+
+
+    return (
+      <React.Fragment>
+        <button
+        onClick={()=> this.props.deleteReport(this.props.match.params.id)}
+        className="ui button negative">
+        Delete
+        </button>
+        <Link to="/reports" className="ui button">Cancel</Link>
+      </React.Fragment>
+    )
+  }
+
+
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    report: state.reports[ownProps.match.params.id]
+  }
+}
+
+export default connect(mapStateToProps,{ fetchReport, deleteReport })(ReportDelete)
