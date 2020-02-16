0e6f6872bd71915a2517638dc7e559cb67466baa
Rich <rpavesi84@gmail.com>
2020-02-16 18:04:43 -0500

    Updates modal css

--- front-client/src/components/GoogleAuth.js
+++ front-client/src/components/GoogleAuth.js
@@ -7,11 +7,10 @@ import  Google_Auth_Key  from './GoogleAuthKey'
 
 
 
-class GoogleAuth extends React.Component{
-
+class GoogleAuth extends React.Component {
 
   componentDidMount(){
-      window.gapi.load('client:auth2', ()=>{
+      window.gapi.load('client:auth2', ()=> {
         window.gapi.client.init({
           clientId: Google_Auth_Key,
           scope: 'email'
