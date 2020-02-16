94b3028e5f1743d9a96c4afe6f0608daae3e6c28
Rich <rpavesi84@gmail.com>
2020-02-16 16:13:27 -0500

    Exports ReportCreate

--- front-client/src/components/reports/ReportCreate.js
+++ front-client/src/components/reports/ReportCreate.js
@@ -3,3 +3,23 @@ Reportimport React from 'react'
 import {connect} from 'react-redux'
 import {createReport} from '../../actions'
 import ReportForm from './ReportForm'
+
+const ReportCreate =(props)=> {
+
+  const onSubmit = (formValues) => {
+    props.createReport(formValues)
+  }
+
+
+
+    return (
+            <div>
+              <h3> Create a Report</h3>
+              <ReportForm onSubmit={onSubmit}/>
+            </div>
+        )
+
+  }
+
+
+export default connect(null,{createReport})(ReportCreate)
