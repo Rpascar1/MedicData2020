a5fa10e5ddef156ce8ea1c04a8fcad67d4f2be37
Rich <rpavesi84@gmail.com>
2020-02-16 16:52:39 -0500

    adds render method to ReportShow component

--- front-client/src/components/reports/ReportShow.js
+++ front-client/src/components/reports/ReportShow.js
@@ -3,6 +3,24 @@ import { connect } from 'react-redux'
 import { fetchReport } from '../../actions'
 
 class ReportShow extends React.Component {
+  componentDidMount(){
+    this.props.fetchReport(this.props.match.params.id)
+  }
+
+render(){
+    if(!this.props.report) {
+      return <div>Loading...</div>
+    }
+      const {title, description} = this.props.report
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+      )
+  }
+}
 
 
 export default connect (mapStateToProps, {fetchReport})(ReportShow)
