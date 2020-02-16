098b8497d2b9a88aac8f3f4590b0138dccccd6dc
Rich <rpavesi84@gmail.com>
2020-02-16 12:16:27 -0500

    fills out the reports_controller with crud actions

--- backend-api-app/app/controllers/api/v1/reports_controller.rb
+++ backend-api-app/app/controllers/api/v1/reports_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::ReportsController < ApplicationController
+
+  def index
+    @reports = Report.all
+    render json: @reports, status: 200
+  end
+
+  def show
+    @report = Report.find(params[:id])
+    render json: @report, status: 200
+  end
+
+  def create
+    @report = Report.create(report_params)
+    render json: @report, status: 200
+  end
+
+  def update
+    @report = Report.find(params[:id])
+    @report.update(report_params)
+    render json: @report, status: 200
+  end
+
+  def destroy
+    @report = Report.find(params[:id])
+    @report.delete
+    render json: {reportId: @report.id}
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :description, :userId, :admin)
+  end
+
+
+
 end
--- backend-api-app/db/seeds.rb
+++ backend-api-app/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Report.create(
+  [
+    {title:"123", description:"123", userId:"asdasd",admin:true},
+    {title:"asdasdasdasd", description:"dsadsadsza", userId:"32423",admin:false},
+    {title:"44444", description:"44444", userId:"55555",admin:false},
+    {title:"234678889", description:"8fghf", userId:"ff",admin:false},
+    {title:"kljkl", description:"cvbcb", userId:"777777",admin:false},
+    {title:"asd", description:"asda8", userId:"888"},
+    {title:"8888888", description:"99999", userId:"000000"}
+  ]
+)
