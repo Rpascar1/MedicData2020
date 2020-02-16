0b378c51c5e03719766fd42c2dc2889ad4daa328
Rich <rpavesi84@gmail.com>
2020-02-16 16:56:17 -0500

    Adds import of report components

--- front-client/src/components/AppRouter.js
+++ front-client/src/components/AppRouter.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import {Router, Route, Switch, Redirect } from 'react-router-dom'
-
 import history from '../history'
+
+import ReportEdit from './reports/ReportEdit'
+import ReportDelete from './reports/ReportDelete'
+import ReportCreate from './reports/ReportCreate'
+import ReportList from './reports/ReportList'
+import ReportShow from './reports/ReportShow'
+
+
+
 
 
 const AppRouter = () => {
